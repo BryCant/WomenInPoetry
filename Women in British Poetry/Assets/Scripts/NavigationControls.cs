7d7cff58c7e8eb7aed21ab7571c5c1bf3ac8690b
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-02 20:58:03 -0600

    PickChoice scene upgraded skeleton, button, navigation controls

---
+++ Women in British Poetry/Assets/Scripts/NavigationControls.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class NavigationControls : MonoBehaviour
+{
+    public Button leftNavigate;
+    public Button rightNavigate;
+
+    public GameObject followObject;
+
+    private int pageNum = 0;
+
+
+    public void MoveCamLeft()
+    {
+        if (pageNum > -1)
+        {
+            pageNum--;
+            Debug.Log("PageNum:" + pageNum.ToString());
+            followObject.transform.position += new Vector3(-19, 0);
+        }
+    }
+
+    public void MoveCamRight()
+    {
+        if (pageNum < 2)
+        {
+            pageNum++;
+            Debug.Log("PageNum:" + pageNum.ToString());
+            followObject.transform.position += new Vector3(19, 0);
+        }
+    }
+}
