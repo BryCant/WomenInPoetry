fb5875ab6e53d8df0db77666d409f6129b94cbca
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-03 15:26:11 -0600

    ScrollBar started; Images switched to default in beginning, then should change to scrollFull based on scrollCount variable

--- Women in British Poetry/Assets/Scripts/RewardReaction.cs
+++ Women in British Poetry/Assets/Scripts/RewardReaction.cs
@@ -8,18 +8,39 @@ using UnityEngine.UI;
 
 public class RewardReaction : MonoBehaviour
 {
+    public Image[] scrolls;
+    public Sprite scrollEmpty;
+    public Sprite scrollFull;
+    private int scrollCount = 0;
+
     public Light2D BuzzLight;
     public GameObject msmsInfo;
     private bool touchable = true;
 
+    private void Start()
+    {
+        foreach(Image scroll in scrolls)
+        {
+            scroll.GetComponent<Image>().sprite = scrollEmpty;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("item_reward") && touchable)
+        if (other.gameObject.CompareTag("scroll") && touchable)
         {
-            touchable = false;
             Destroy(other.gameObject);
-            Debug.Log("lol");
-            Debug.Log("lipop");
+            touchable = false;
+
+            scrollCount++;
+
+            switch (scrollCount)
+            {
+                case 1:
+                    scrolls[1].GetComponent<Image>().sprite = scrollFull;
+                    break;
+            }
+
             if (msmsInfo.activeSelf)
             {
                 msmsInfo.SetActive(false);
