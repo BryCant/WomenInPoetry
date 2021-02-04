3de4b9cd4317aed8f32bd4083adbd6216ba8f49e
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-03 23:51:18 -0600

    Tae A Moose
    Lots of mouse struggle for no reason; Added mouse with trail

---
+++ Women in British Poetry/Assets/Scripts/CursorBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class CursorBehavior : MonoBehaviour
+{
+    //public float timeBtwnSpwn = 0.1f;
+   // public GameObject trailEffect;
+    public float mouseSensitivity = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorPos.x *= mouseSensitivity;
+        cursorPos.y *= mouseSensitivity;
+        transform.position = cursorPos;
+
+        //if(timeBtwnSpwn <= 0)
+        //{
+        //    Instantiate(trailEffect, cursorPos, Quaternion.identity);
+        //    timeBtwnSpwn = 0.1f;
+        //}
+        //else
+        //{
+        //    timeBtwnSpwn -= Time.deltaTime;
+        //}
+    }
+}
