f332b7f2a99d6ecf56c52830cbb006d3414fb36e
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-04 23:45:24 -0600

    Williams Before Death
    Death sprite is in and set up with animations but script is incomplete

---
+++ Women in British Poetry/Assets/Scripts/ReaperBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperBehavior : MonoBehaviour
+{
+    //public string objectTag;
+    public float speed;
+    public float multiplyer;
+    //public float sightDistance;
+    
+    //private bool movingLeft = true;
+
+    //public Transform groundSensor;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        speed *= multiplyer;
+
+        /*int layer_mask = LayerMask.GetMask(objectTag);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundSensor.position, Vector2.down, sightDistance, layer_mask);
+        Debug.Log(groundInfo.collider);
+        if (groundInfo.collider == true)
+        {
+            Debug.Log("hit");
+            if (movingLeft == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingLeft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingLeft = true;
+            }
+        }
+        else if (groundInfo.collider == true)
+        {
+            Debug.Log("not hit");
+        }*/
+    }
+}
