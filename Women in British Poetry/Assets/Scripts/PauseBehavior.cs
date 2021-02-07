c01e080b86454cbfcdde4782c79a635eea7da8c4
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-07 13:20:22 -0600

    Skeleton Pause Menu Added

---
+++ Women in British Poetry/Assets/Scripts/PauseBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBehavior : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void OnPause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void OnContinue()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void OnSoundToggle()
+    {
+
+    }
+
+    public void OnExit()
+    {
+
+    }
+}
