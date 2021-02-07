325f4a7bf14d704f280114781b4ad240f524c7f5
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-06 23:37:37 -0600

    Reaper Be Flipping
    A* added to prefab and montagu

---
+++ Women in British Poetry/Assets/Scripts/ReaperFlip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ReaperFlip : MonoBehaviour
+{
+    public AIPath aiPath;
+    private float currScale;
+
+    private void Start()
+    {
+        currScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(currScale, currScale, currScale);
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-currScale, currScale, currScale);
+        }
+    }
+}
