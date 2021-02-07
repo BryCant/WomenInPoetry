3fa559b61c529db027ece750f082c7519cf760fd
Bryant <33697753+BryCant@users.noreply.github.com>
2021-02-07 00:40:45 -0600

    she be dying

---
+++ Women in British Poetry/Assets/Scripts/DeathReaction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathReaction : MonoBehaviour
+{
+    public Animator playerAnim;
+
+    private bool isFrozen = false;
+    private Vector3 currPos;
+
+    private void Update()
+    {
+        if (isFrozen)
+        {
+            transform.position = currPos;
+            //aiPath.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("death"))
+        {
+            playerAnim.SetBool("isDying", true);
+            isFrozen = true;
+            currPos = transform.position;
+        }
+    }
+}
